a17ee9a25ab77e71bea9ea3d61c02d274ee0de86
abedassi2001 <142339823+abedassi2001@users.noreply.github.com>
2026-04-24 11:48:56 +0300

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0 
+        max_length = 0
+        s_map = {}
+
+        for i in range(len(s)):
+            char = s[i]
+
+            # Logic: If we find a duplicate, clean the map from 'start' up to the duplicate
+            if char in s_map:
+                to_del_index = s_map[char]
+                
+                # IMPORTANT: Delete everything from the current start up to the duplicate
+                # This physically shrinks your "window" in the dictionary
+                for j in range(start, to_del_index):
+                    del s_map[s[j]]
+                
+                # Move start to one position after the old duplicate
+                start = to_del_index + 1
+            
+            # Update/Insert the current character
+            s_map[char] = i
+            
+            # Logic: The window size is simply (current_index - start + 1)
+            current_len = i - start + 1
+            max_length = max (max_length , current_len)
+
+        return max_length
